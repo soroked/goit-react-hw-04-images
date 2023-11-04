e3382bdcf66acfb761c4f1ff901faa5333570d2b
Eduard <sorokolietov@gmail.com>
2023-11-05 00:04:45 +0100

    commit

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,40 +1,37 @@
-import { Component } from 'react';
+import { useEffect } from 'react';
 
-export default class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.closeModal);
+const Modal = ({ image, handleCloseModal }) => {
+  useEffect(() => {
+    window.addEventListener('keydown', closeModal);
     document.body.style.overflow = 'hidden';
-    // by clicking on a pic a window scrolls to the top
-    // document.body.style.position = 'fixed';
-  }
 
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.closeModal);
-    document.querySelector('body').style.overflow = 'auto';
-    // document.querySelector('body').style.position = 'absolute';
-  }
+    return () => {
+      window.removeEventListener('keydown', closeModal);
+      document.body.style.overflow = 'auto';
+    };
+  }, []);
 
-  closeModal = e => {
+  const closeModal = e => {
     if (e.target.className === 'Overlay') {
-      this.props.handleCloseModal();
+      handleCloseModal();
       return;
     }
 
     if (e.code === 'Escape') {
-      this.props.handleCloseModal();
+      handleCloseModal();
       return;
     }
   };
 
-  render() {
-    const { largeImageURL, tags } = this.props.image;
+  const { largeImageURL, tags } = image;
 
-    return (
-      <div className="Overlay" onClick={this.closeModal}>
-        <div className="Modal">
-          <img src={largeImageURL} alt={tags} />
-        </div>
+  return (
+    <div className="Overlay" onClick={closeModal}>
+      <div className="Modal">
+        <img src={largeImageURL} alt={tags} />
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+export default Modal;
